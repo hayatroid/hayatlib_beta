9188ee3b5e5405d19af253bb882a02bb5e50f393
hayatroid <hayatroid@users.noreply.github.com>
2024-05-10 05:43:53 +0000

    deploy: 4f9d1b8ec84b6f229b9f9d9c7350ea20ec41bd46

--- trait.impl/core/clone/trait.Clone.js
+++ trait.impl/core/clone/trait.Clone.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {
-"hayatlib":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.77.1/core/clone/trait.Clone.html\" title=\"trait core::clone::Clone\">Clone</a> for <a class=\"struct\" href=\"hayatlib/polynomial/struct.FPS.html\" title=\"struct hayatlib::polynomial::FPS\">FPS</a>"]]
+"hayatlib":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.77.2/core/clone/trait.Clone.html\" title=\"trait core::clone::Clone\">Clone</a> for <a class=\"struct\" href=\"hayatlib/polynomial/struct.FPS.html\" title=\"struct hayatlib::polynomial::FPS\">FPS</a>"]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
--- trait.impl/core/ops/arith/trait.Sub.js
+++ trait.impl/core/ops/arith/trait.Sub.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {
-"hayatlib":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.77.1/core/ops/arith/trait.Sub.html\" title=\"trait core::ops::arith::Sub\">Sub</a> for <a class=\"struct\" href=\"hayatlib/polynomial/struct.FPS.html\" title=\"struct hayatlib::polynomial::FPS\">FPS</a>"]]
+"hayatlib":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.77.2/core/ops/arith/trait.Sub.html\" title=\"trait core::ops::arith::Sub\">Sub</a> for <a class=\"struct\" href=\"hayatlib/polynomial/struct.FPS.html\" title=\"struct hayatlib::polynomial::FPS\">FPS</a>"]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
